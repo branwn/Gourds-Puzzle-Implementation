6ed1ba53d0987c3d6e305273a2a89d9fc4c99c22
branwn <50729260+branwn@users.noreply.github.com>
2020-11-25 17:12:41 +0800

    Update gourdsBoard.py

--- gourdsBoard.py
+++ gourdsBoard.py
@@ -1,23 +1,30 @@
 import pygame
 import numpy as np  # for matrix
 
+pygame.init()
+# size of the window
+screen = pygame.display.set_mode((800, 600))
+# caption setting
+pygame.display.set_caption('Gourds')
+# background colour setting
+screen.fill((242, 242, 242))
+# set width of the hexagonal cell
+widthOfHexCell = 80;
+offset = widthOfHexCell*1.7
+
+
+# set a matrix of board
+matrixOfBoard = np.array([[1, 0, 3], [0, 5, 0]])
+
+# set a initial gourds placement
+gourdsPlacement = np.array([[[1,1],[2,2],[255,0,0],[0,255,0]]])
+gourdsColour = np.array([[[1,1],[2,2],[255,0,0],[0,255,0]]])
+
 def boardConstructor():
-    pygame.init()
-    # 初始化用于显示的窗口并设置窗口尺寸
-    screen = pygame.display.set_mode((800, 600))
-    # caption setting
-    pygame.display.set_caption('Gourds')
-    # background colour setting
-    screen.fill((242, 242, 242))
-    # set width of the hexagonal cell
-    widthOfHexCell = 80;
-    # set a matrix with offsets in even rows
-    nd_two = np.array([[1, 0, 3], [0, 5, 0]])
-    offset = widthOfHexCell*1.7
-    for i in range(len(nd_two)):
-        for j in range(len(nd_two[0])):
+    for i in range(len(matrixOfBoard)):
+        for j in range(len(matrixOfBoard[0])):
             pygame.draw.circle(screen,(0,0,0),(offset + j * widthOfHexCell, offset + i * widthOfHexCell * 1.732) ,6,1)
-            if(nd_two[i][j]):
+            if(matrixOfBoard[i][j]):
                 # draw a hexagonal cell
                 pygame.draw.polygon(screen, (100, 100, 200,),
                                     [(offset + j * widthOfHexCell, offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 1.1547), (
@@ -31,9 +38,16 @@ def boardConstructor():
                                      offset + j * widthOfHexCell - widthOfHexCell,
                                      offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 0.57735)], 1)
 
-    # 刷新当前窗口(渲染窗口将绘制的图像呈现出来)
+    # refresh the window
     pygame.display.flip()
 
+def gourdsConstructor():
+    for i in range(len(gourdsPlacement)):
+        pygame.draw.circle(screen,(255,0,0),(offset + 1 * widthOfHexCell, offset + i * widthOfHexCell * 1.732) ,widthOfHexCell*0.6,0)
+        print(i[0][0])
+    # refresh the window
+    pygame.display.flip()
+    
 def main():
     # 开启一个事件循环处理发生的事件
     running = True
@@ -46,4 +60,5 @@ def main():
 
 if __name__ == '__main__':
     boardConstructor()
+    gourdsConstructor()
     main()
