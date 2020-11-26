fde7e4003a5b3f716f780f96ea277b62347ba28b
branwn <50729260+branwn@users.noreply.github.com>
2020-11-26 15:26:31 +0800

    bugs fixed

--- gourdsBoard.py
+++ gourdsBoard.py
@@ -18,7 +18,7 @@ gourdSize = widthOfHexCell * 0.6
 matrixOfBoard = np.array([[0, 0, 0, 0, 0, 0, 0],
                           [0, 0, 1, 0, 1, 0, 0],
                           [0, 0, 0, 1, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0]])
+                          ])
 
 # set a initial gourds placement
 #                           x, y, x, y
@@ -28,28 +28,29 @@ gourdsLocation = np.array([[2, 1, 3, 2]])
 def boardConstructor():
     font = pygame.font.Font('OpenSans-Light.ttf', 16)
 
+
     for i in range(len(matrixOfBoard)):
         for j in range(len(matrixOfBoard[0])):
             # display matrix
             text = font.render(str(matrixOfBoard[i][j]), True, (0, 0, 255))
-            screen.blit(text, (-4 + offset + j * widthOfHexCell, -12 + offset + i * widthOfHexCell * 1.732))
+            screen.blit(text, (-4 + offset + j * widthOfHexCell, int(-12 + offset + i * widthOfHexCell * 1.732)))
             # pygame.draw.circle(screen,(0,0,0),(offset + j * widthOfHexCell, offset + i * widthOfHexCell * 1.732) ,6,1)
 
             if (matrixOfBoard[i][j]):
                 # draw a hexagonal cell
                 pygame.draw.polygon(screen, (100, 100, 200,),
                                     [(offset + j * widthOfHexCell,
-                                      offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 1.1547), (
+                                      int(offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 1.1547)), (
                                          offset + j * widthOfHexCell + widthOfHexCell,
-                                         offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 0.57735), (
+                                         int(offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 0.57735)), (
                                          offset + j * widthOfHexCell + widthOfHexCell,
-                                         offset + i * widthOfHexCell * 1.732 - widthOfHexCell * 0.57735),
+                                         int(offset + i * widthOfHexCell * 1.732 - widthOfHexCell * 0.57735)),
                                      (offset + j * widthOfHexCell,
-                                      offset + i * widthOfHexCell * 1.732 - widthOfHexCell * 1.1547),
+                                      int(offset + i * widthOfHexCell * 1.732 - widthOfHexCell * 1.1547)),
                                      (offset + j * widthOfHexCell - widthOfHexCell,
-                                      offset + i * widthOfHexCell * 1.732 - widthOfHexCell * 0.57735), (
+                                      int(offset + i * widthOfHexCell * 1.732 - widthOfHexCell * 0.57735)), (
                                          offset + j * widthOfHexCell - widthOfHexCell,
-                                         offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 0.57735)], 1)
+                                         int(offset + i * widthOfHexCell * 1.732 + widthOfHexCell * 0.57735))], 1)
 
     # refresh the window
     pygame.display.flip()
@@ -58,15 +59,15 @@ def boardConstructor():
 def gourdsConstructor():
     for i in range(len(gourdsLocation)):
         pygame.draw.circle(screen, (100, 0, 0), (
-            offset + gourdsLocation[i][0] * widthOfHexCell, offset + gourdsLocation[i][1] * widthOfHexCell * 1.732),
+            int(offset + gourdsLocation[i][0] * widthOfHexCell), int(offset + gourdsLocation[i][1] * widthOfHexCell * 1.732)),
                            gourdSize, 1)
         pygame.draw.circle(screen, (100, 0, 0), (
-            offset + gourdsLocation[i][2] * widthOfHexCell, offset + gourdsLocation[i][3] * widthOfHexCell * 1.732),
+            int(offset + gourdsLocation[i][2] * widthOfHexCell), int(offset + gourdsLocation[i][3] * widthOfHexCell * 1.732)),
                            gourdSize, 1)
         pygame.draw.line(screen, (100, 0, 0), (
-            offset + gourdsLocation[i][0] * widthOfHexCell, offset + gourdsLocation[i][1] * widthOfHexCell * 1.732), (
+            offset + gourdsLocation[i][0] * widthOfHexCell, int(offset + gourdsLocation[i][1] * widthOfHexCell * 1.732)), (
                              offset + gourdsLocation[i][2] * widthOfHexCell,
-                             offset + gourdsLocation[i][3] * widthOfHexCell * 1.732), width=1)
+                             int(offset + gourdsLocation[i][3] * widthOfHexCell * 1.732)), width=2)
     # refresh the window
     pygame.display.flip()
 
@@ -102,46 +103,58 @@ def searchGourdsFromIndex(x, y, xDestination = -1, yDestination = -1):
 
 
 def searchAnEmptyCellAround(x, y):
-    # search an empty cell around x, y
+    # obtain the size of the boardMatrix
+    maxOfX = len(matrixOfBoard[0]) - 1
+    maxOfY = len(matrixOfBoard) - 1
 
-    if (matrixOfBoard[y - 1][x - 1] == 1) and (searchGourdsFromIndex(x - 1, y - 1) == (-1, -1)):  # upper left
-        return x - 1, y - 1
-    if (matrixOfBoard[y + 1][x - 1] == 1) and (searchGourdsFromIndex(x - 1, y + 1) == (-1, -1)):  # lower left
-        return x - 1, y + 1
-    if (matrixOfBoard[y + 1][x + 1] == 1) and (searchGourdsFromIndex(x + 1, y + 1) == (-1, -1)):  # lower right
-        return x + 1, y + 1
-    if (matrixOfBoard[y - 1][x + 1] == 1) and (searchGourdsFromIndex(x + 1, y - 1) == (-1, -1)):  # upper right
-        return x + 1, y - 1
-    if (matrixOfBoard[y][x - 2] == 1) and (searchGourdsFromIndex(x - 2, y) == (-1, -1)):  # left
-        return x - 2, y
-    if (matrixOfBoard[y][x + 2] == 1) and (searchGourdsFromIndex(x + 2, y) == (-1, -1)):  # right
-        return x + 2, y
+    # search an empty cell around x, y
+    if x - 1 >= 0 and y - 1 >= 0:
+        if (matrixOfBoard[y - 1][x - 1] == 1) and (searchGourdsFromIndex(x - 1, y - 1) == (-1, -1)):  # upper left
+            return x - 1, y - 1
+    if x - 1 >= 0 and y + 1 <= maxOfY:
+        if (matrixOfBoard[y + 1][x - 1] == 1) and (searchGourdsFromIndex(x - 1, y + 1) == (-1, -1)):  # lower left
+            return x - 1, y + 1
+    if x + 1 <= maxOfX and y + 1 <= maxOfY:
+        if (matrixOfBoard[y + 1][x + 1] == 1) and (searchGourdsFromIndex(x + 1, y + 1) == (-1, -1)):  # lower right
+            return x + 1, y + 1
+    if x + 1 <= maxOfX and y - 1 >= 0:
+        if (matrixOfBoard[y - 1][x + 1] == 1) and (searchGourdsFromIndex(x + 1, y - 1) == (-1, -1)):  # upper right
+            return x + 1, y - 1
+    if x - 2 >= 0:
+        if (matrixOfBoard[y][x - 2] == 1) and (searchGourdsFromIndex(x - 2, y) == (-1, -1)):  # left
+            return x - 2, y
+    if x + 2 <= maxOfX:
+        if (matrixOfBoard[y][x + 2] == 1) and (searchGourdsFromIndex(x + 2, y) == (-1, -1)):  # right
+            return x + 2, y
     return -1, -1
 
 
+def mouseClick(pos):
+    xGourd, yGourd = searchGourdsFromCoordinate(pos)
+    if xGourd != -1:
+        print(xGourd, yGourd)
+        xCell, yCell = searchAnEmptyCellAround(xGourd, yGourd)
+        if xCell != -1:
+            searchGourdsFromIndex(xGourd, yGourd, xCell, yCell)
+            screen.fill((242, 242, 242))
+            boardConstructor()
+            gourdsConstructor()
+            pygame.display.flip()
+
+
 def main():
-    # 开启一个事件循环处理发生的事件
+    # main loop
     running = True
     while running:
-        # 从消息队列中获取事件并对事件进行处理
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
             # MOUSE BUTTON DOWN
             if event.type == pygame.MOUSEBUTTONDOWN:
-                xGourd, yGourd = searchGourdsFromCoordinate(event.pos)
-                if xGourd != -1:
-                    print(xGourd, yGourd)
-                    xCell, yCell = searchAnEmptyCellAround(xGourd, yGourd)
-                    if xCell != -1:
-                        searchGourdsFromIndex(xGourd, yGourd, xCell, yCell)
-                        screen.fill((242, 242, 242))
-                        boardConstructor()
-                        gourdsConstructor()
-                        pygame.display.flip()
-
-            # 鼠标弹起
+                mouseClick(event.pos)
+            # MOUSE BUTTON UP
             if event.type == pygame.MOUSEBUTTONUP:
                 pass
 
